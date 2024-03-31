59e20db6b3802d38910efe6cdf0890ded9d9c8fd
David Li <dli7319@gmail.com>
2024-03-31 01:51:56 -0400

    Create jpdb_enter_show_answer.js

---
+++ jpdb_enter_show_answer.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         JPDB review: Press enter to show answer.
+// @namespace    http://tampermonkey.net/
+// @version      2024-03-31
+// @description  Press enter to show answer.
+// @author       You
+// @match        https://jpdb.io/review
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=jpdb.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function showAnswer() {
+        const showAnswerButton = document.getElementById("show-answer");
+        if (showAnswerButton) {
+            showAnswerButton.click();
+        }
+    }
+
+    document.addEventListener("keypress", (event) => {
+        if (event.key === 'Enter') {
+            showAnswer();
+        }
+    });
+})();
